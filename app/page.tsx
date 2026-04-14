e72e8e145eed2e16f23b376e2c31383fdaaef41b
Cece Abdurahman <cece.abdurahman@gmail.com>
2026-04-14 11:50:12 +0700

    Add Home component to page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to MikroLiving CMS</h1>
+      <p>Backend is running with Node.js + Express + MySQL</p>
+    </main>
+  )
+}
